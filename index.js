8e92c3c2bf401899d639e5e8c9555b3b4c5a41a2
Meecore4 <[alicia.critelli4@gmail.com](mailto:alicia.critelli4@gmail.com)>
2022-12-08 09:46:26 +0100

    CA MARCHE git add .!

--- index.js
+++ index.js
@@ -4,80 +4,93 @@
 
 const chrono = {
 
-    minutes : 0,
+    minutes: 0,
     seconds: 0,
     tenthOfSeconds: 0,
 
     //     - Déclarer une méthode `display` qui va permettre de mettre à jour les div contenant les minutes, les secondes et les millisecondes.
 
-display: function () {
+    display: function () {
 
-    const mesMinutes = document.querySelector('.myMinutes');
-    mesMinutes.textContent = chrono.minutes;
+        if(chrono.minutes < 10){
+            chrono.minutes = '0' + parseInt(chrono.minutes);
+        }
 
-    const mesSeconds = document.querySelector('.mySeconds');
-    mesSeconds.textContent = chrono.seconds;
+        const mesMinutes = document.querySelector('.myMinutes');
+        mesMinutes.textContent = chrono.minutes;
 
-    const mesTenthOfSeconds = document.querySelector('.myTenthOfSeconds');
-    mesTenthOfSeconds.textContent = chrono.tenthOfSeconds;
+      
+        if (chrono.seconds < 10) {
+            chrono.seconds = '0' + parseInt(chrono.seconds);
+        }
+
+          const mesSeconds = document.querySelector('.mySeconds');
+        mesSeconds.textContent = chrono.seconds;
+
+        if (chrono.tenthOfSeconds < 10) {
+            chrono.tenthOfSeconds = '0' + parseInt(chrono.tenthOfSeconds);
+        }
+
+        const mesTenthOfSeconds = document.querySelector('.myTenthOfSeconds');
+        mesTenthOfSeconds.textContent = chrono.tenthOfSeconds;
 
     },
 
-start: function () {
-    //     - Utiliser la fonction setInterval()\* permet de répéter le code de la méthode changeTheTime tous les x 1/1000 de secondes.
+    start: function () {
+        //     - Utiliser la fonction setInterval()\* permet de répéter le code de la méthode changeTheTime tous les x 1/1000 de secondes.
         setInterval(chrono.changeTheTime, 100);
-   },
+    },
 
 
-//     - Déclarer une méthode `changeTheTime` qui va gérer l'ajout d' 1/10 de seconde et éventullement modifier la valeur de secondes et de minutes
+    //     - Déclarer une méthode `changeTheTime` qui va gérer l'ajout d' 1/10 de seconde et éventullement modifier la valeur de secondes et de minutes
 
-changeTheTime: function () {
+    changeTheTime: function () {
 
-    chrono.tenthOfSeconds++;
+        chrono.tenthOfSeconds++;
 
-    if(chrono.tenthOfSeconds === 10){
-        chrono.seconds++;
-        chrono.tenthOfSeconds = 0;
+        if (chrono.tenthOfSeconds === 10) {
+            chrono.seconds++;
+            chrono.tenthOfSeconds = 0;
 
-    }
+        }
 
-    if(chrono.seconds === 60){
-        chrono.minutes++;
-        chrono.seconds = 0;
-    }
+        if (chrono.seconds === 60) {
+            chrono.minutes++;
+            chrono.seconds = 0;
+        }
 
-    chrono.display();
-},
-
-
-// - Déclarer **une méthode `init`** qui va gérer la création et la configuration des éléments dans le DOM.
-//  - Déclarer la méthode  dans l'objet chrono.
-        
-        
-init: function () {
-
-    const chronometre = document.querySelector('#chrono');
-
-    const myMinutes = document.createElement('div');
-    myMinutes.classList.add('myMinutes');
-    chronometre.appendChild('myMinutes');
-    myMinutes.textContent = chrono.minutes;
-
-    const mySeconds = document.createElement('div');
-    mySeconds.classList.add('mySeconds');
-    chronometre.appendChild('mySeconds');
-    mySeconds.textContent = chrono.seconds;
-
-    const myTenthOfSeconds = document.createElement('div');
-    myTenthOfSeconds.classList.add('myTenthOfSeconds');
-    chronometre.appendChild('myTenthOfSeconds');
-    myTenthOfSeconds.textContent = chrono.tenthOfSeconds;
+        chrono.display();
+    },
 
 
-    chrono.start();
-    
-    
-},
+    // - Déclarer **une méthode `init`** qui va gérer la création et la configuration des éléments dans le DOM.
+    //  - Déclarer la méthode  dans l'objet chrono.
+
+
+    init: function () {
+
+        const chronometre = document.querySelector('#chrono');
+
+        const myMinutes = document.createElement('div');
+        myMinutes.classList.add('myMinutes');
+        chronometre.appendChild(myMinutes);
+        myMinutes.textContent = chrono.minutes;
+
+        const mySeconds = document.createElement('div');
+        mySeconds.classList.add('mySeconds');
+        chronometre.appendChild(mySeconds);
+        mySeconds.textContent = chrono.seconds;
+
+        const myTenthOfSeconds = document.createElement('div');
+        myTenthOfSeconds.classList.add('myTenthOfSeconds');
+        chronometre.appendChild(myTenthOfSeconds);
+        myTenthOfSeconds.textContent = chrono.tenthOfSeconds;
+
+
+        chrono.start();
+
+
+    },
 }
 
 
